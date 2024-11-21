6a7ab9675ec91afa8a0c30693e2dc9c6579a86b1
victoramelinckx <vickx2000@gmail.com>
2024-11-21 14:04:53 -0500

    Heroo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
     <section className="flex flex-col relative w-full justify-center items-center ">
       {/* Background Image Container */}
       <div className="absolute inset-0 w-full h-full z-0">
-        <div className="absolute inset-0 bg-black/60 z-[1]" /> {/* Overlay */}
+        <div className="absolute inset-0 bg-black/70 z-[1]" /> {/* Overlay */}
         <Image
           src="/background.png"
           alt="Construction background"
@@ -22,7 +22,7 @@ export const Hero = () => {
       </div>
 
       {/* Content Container - Note the z-10 to place it above the background */}
-      <div className="relative z-10 flex max-lg:flex-col lg:py-[150px] w-full lg:w-11/12 max-w-6xl lg:pb-6 gap-14 lg:gap-6 lg:justify-between">
+      <div className="relative z-10 flex max-lg:flex-col lg:py-[140px]  w-full lg:w-11/12 max-w-6xl gap-14 lg:gap-6 lg:justify-between">
         <div className="flex flex-col justify-center text-white md:max-lg:items-center gap-3 sm:gap-6 lg:gap-4 max-w-4xl md:max-lg:text-center max-lg:px-6 mr-10">
           <h1 className="leading-tight tracking-[.01em] font-dmsans font-bold text-[44px] md:text-5xl lg:text-6xl ">
             Innovación y experiencia
