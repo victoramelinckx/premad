776ea56da14de6400bb13cbfdacbb0b530a2f4f0
victoramelinckx <vickx2000@gmail.com>
2024-11-21 23:28:57 -0500

    Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,18 +22,18 @@ export const Hero = () => {
       </div>
 
       {/* Content Container - Note the z-10 to place it above the background */}
-      <div className="relative z-10 flex max-lg:flex-col lg:py-[140px]  w-full lg:w-11/12 max-w-6xl gap-14 lg:gap-6 lg:justify-between">
+      <div className="relative z-10 flex max-lg:flex-col max-md:pt-28 lg:py-[140px]  w-full lg:w-11/12 max-w-6xl gap-14 lg:gap-6 lg:justify-between">
         <div className="flex flex-col justify-center text-white md:max-lg:items-center gap-3 sm:gap-6 lg:gap-4 max-w-4xl md:max-lg:text-center max-lg:px-6 mr-10">
-          <h1 className="leading-tight tracking-[.01em] font-dmsans font-bold text-[44px] md:text-5xl lg:text-6xl ">
+          <h1 className="leading-tight tracking-[.01em] font-dmsans max-md:text-center font-bold text-[44px] md:text-5xl lg:text-6xl ">
             Innovación y experiencia
-            <br />
+            <br className="hidden lg:flex" />
             en construcción industrial.
           </h1>
-          <p className=" md:leading-normal tracking-[.03em] max-sm:font-medium font-[450] text-base max-w-lg  text-white sm:max-lg:w-4/5">
+          <p className=" md:leading-normal max-md:text-center tracking-[.03em] max-sm:font-medium font-[450] text-base max-w-lg  text-white sm:max-lg:w-4/5">
             Más de 20 años transformando la construcción modular con soluciones
             industrializadas de alto rendimiento para proyectos a gran escala.
           </p>
-          <div className="flex flex-row space-x-2 mt-4">
+          <div className="flex flex-col lg:flex-row max-md:items-center space-x-2 mt-4">
             <button className="mt-3 text-lg flex items-center gap-1 rounded-full bg-primary-500 px-5 py-3 text-white font-semibold w-fit hover:opacity-70">
               Nuestros servicios
             </button>
