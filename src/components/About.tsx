f760d62a7c523f2d513ecb0787ef04cf04aa828b
victoramelinckx <vickx2000@gmail.com>
2024-12-21 11:50:55 -0500

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,9 +8,9 @@ export const About = () => {
   return (
     <Element name="acerca" className="w-full flex justify-center">
       <div className=" my-10 lg:my-20 w-full max-w-6xl px-8 md:px-4">
-        <section className="flex flex-col-reverse lg:flex-row items-center gap-8 lg:gap-12">
+        <section className="flex flex-col-reverse lg:flex-row items-center gap-8 lg:gap-16">
           {/* Contenedor del texto - Ahora con flex y centrado vertical */}
-          <div className="w-full lg:w-1/2 flex flex-col justify-center">
+          <div className="w-full lg:w-[45%] flex flex-col justify-center">
             <div>
               <h3 className="lg:text-[48px]  text-[36px] leading-tight font-bold">
                 ¿Quiénes somos?
@@ -26,18 +26,31 @@ export const About = () => {
                 metodología integrada garantiza resultados excepcionales desde
                 el diseño hasta la ejecución final.
               </p>
-              <button className=" mt-[30px] lg:mt-10 text-lg flex items-center gap-1 rounded-full bg-primary-500 px-5 py-3 text-white font-semibold w-fit hover:opacity-70">
+              <button className=" mt-[30px] lg:mt-10 text-lg flex items-center gap-1 rounded-full bg-white/30 blur-xs border-solid border-2  px-6 py-3 text-black border-black font-semibold w-fit hover:opacity-70">
                 Nuestros proyectos
               </button>
+              {/* <Link
+                key="contacto"
+                to="contacto"
+                smooth
+                spy
+                offset={-72}
+                className=""
+              >
+                <Button
+                  title="Contáctenos"
+                  className="mt-3  bg-white/30 blur-xs border-solid  border-2 px-6 py-3 text-white border-white font-semibold w-fit hover:opacity-70"
+                />
+              </Link> */}
             </div>
           </div>
 
           {/* Contenedor de la imagen - Altura automática */}
-          <div className="w-full lg:w-1/2">
+          <div className="w-full lg:w-[55%]">
             <Image
               src={Acercade}
               alt="About us image"
-              className="w-full rounded-2xl shadow-lg"
+              className="w-full rounded-2xl shadow-xl"
               priority
               placeholder="blur"
             />
